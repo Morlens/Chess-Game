308afe9eb16cb6300665e4f7bd9341c1849381cd
desart <domdeorboc@gmail.com>
2026-04-14 14:38:06 +0800

    added rook movement logic

--- index.js
+++ index.js
@@ -119,11 +119,16 @@ function movePiece(from, to) {
             enPassantSquare = "";
         }
     }
-
+    
+    //call moveBishop() func if selected gamecell is bishop 
     if(selectedPiece.toLowerCase() === 'b'){
         if(!moveBishop(fc,fr,tc,tr)) return;
     }
 
+    //call moveRook() func if selected gamecell is Rook
+    if(selectedPiece.toLowerCase() === 'r'){
+        if(!moveRook(fc,fr,tc,tr)) return;
+    }
 
 
     board[toCellIndex] = selectedPiece;
@@ -191,7 +196,29 @@ function moveBishop(fc, fr, tc, tr){
     return true;
 
 }
+
+function moveRook(fc, fr, tc, tr){
+
+    // 1 up, -1 down, 0 if moving horizontally
+    // 1 right, -1 left, 0 if moving vertically
+    let rowDirection = tr > fr ? 1 : tr < fr ? -1 : 0
+    let colDirection = tc > fc ? 1 : tc < fc ? -1 : 0
     
+    //rook direction
+    let r = fr + rowDirection;
+    let c = fc + colDirection;
+    
+    if(tc !== fc && tr === fr || tc === fc && tr !== fr){
+     while(r === tr && c !== tc || r !== tr && c === tc){
+        let index = (8 - r) * 8 + (c - 1);
+        if(board[index] !== "")return false;
+        r += rowDirection;
+        c += colDirection;
+     }
+    return true;
+    }
+  return false;
+}
 
  renderBoard();
 
