63e7ccc5629f953363d9154a49aa187cc31fc267
desart <domdeorboc@gmail.com>
2026-04-17 20:34:50 +0800

    added loop for detection of enemy piece.

--- js/checkLogic.js
+++ js/checkLogic.js
@@ -5,7 +5,16 @@ function isInCheck(color) {
     let kingRow = 8 - Math.floor(kingIndex / 8);
     let kingColumn = (kingIndex % 8) + 1
 
-   
+    for(let i = 0; i < board.length; i++){
+        let square = board[i];
+
+        if(square === "") continue; //skip empty squares
+
+        if(color === "white" && square === square.toLowerCase() || color === "black" &&
+        square === square.toUpperCase()){
+            console.log("Found enemy piece");
+        }
+    }
 }
  
 function isCheckmate(color) {
